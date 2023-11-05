f760492e5a57ebec249e14200c3038353fd924eb
godhyzzang <amksh0914@gmail.com>
2023-11-05 23:10:22 +0900

    실습 2-2

--- ex1105/main.c
+++ ex1105/main.c
@@ -6,15 +6,20 @@
 int main(void) {
 
 	int grade[N_STUDENT];
-	int i;
-	printf("inpit 5 scores");
+	int i,average;
+	int sum;
+	printf("input 5 scores");
 
-
+	sum = 0;
 	for (i = 0; i < N_STUDENT; i++)
+	{
 		scanf_s("%d", &grade[i]);
+		sum += grade[i];
+	}
 
-	for (i = 0; i < N_STUDENT; i++)
-		printf("grade[%i] = %i\n", i, grade[i]);
+	average = sum / N_STUDENT;
+		
+	printf("score average : %i\n" , average);
 
 
 	return 0;
